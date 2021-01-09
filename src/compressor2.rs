6730581b1ddc40e4e936d498f80af1481289264a
DGriffin91 <git@dgdigital.net>
2021-01-09 13:00:42 -0800

    refactor

--- src/compressor2.rs
+++ src/compressor2.rs
@@ -63,27 +63,21 @@ impl VariableRingBuf {
 }
 
 struct DecoupledPeakDetector {
-    attack_input: f32,
-    release_input: f32,
     attack: f32,
     release: f32,
     env: f32,
     env2: f32,
-    sample_rate: f32,
 }
 
 impl DecoupledPeakDetector {
     pub fn new(attack: f32, release: f32, sample_rate: f32) -> DecoupledPeakDetector {
         let mut detector = DecoupledPeakDetector {
-            attack_input: attack,
-            release_input: release,
             attack: 0.0,
             release: 0.0,
             env: 0.0,
             env2: 0.0,
-            sample_rate,
         };
-        detector.update();
+        detector.update(attack, release, sample_rate);
         detector
     }
 
@@ -106,24 +100,45 @@ impl DecoupledPeakDetector {
         self.env2
     }
 
-    fn update(&mut self) {
-        self.attack = (-1.0 * PI * 1000.0 / self.attack_input / self.sample_rate).exp();
-        self.release = (-1.0 * PI * 1000.0 / self.release_input / self.sample_rate).exp();
+    fn update(&mut self, attack: f32, release: f32, sample_rate: f32) {
+        self.attack = (-1.0 * PI * 1000.0 / attack / sample_rate).exp();
+        self.release = (-1.0 * PI * 1000.0 / release / sample_rate).exp();
     }
+}
 
-    fn set_sample_rate(&mut self, sample_rate: f32) {
-        self.sample_rate = sample_rate;
-        self.update();
+struct AccumulatingRMS {
+    buffer: VariableRingBuf,
+    end: f32,
+    new: f32,
+    rms: f32,
+}
+
+impl AccumulatingRMS {
+    fn new(sample_rate: usize, rms_size_ms: f32, rms_max_size_samp: usize) -> AccumulatingRMS {
+        AccumulatingRMS {
+            buffer: VariableRingBuf::new(
+                ((sample_rate as f32) * (rms_size_ms / 1000.0)) as usize,
+                rms_max_size_samp,
+            ),
+            end: 0.0,
+            new: 0.0,
+            rms: 0.0,
+        }
     }
-
-    fn set_attack(&mut self, attack: f32) {
-        self.attack_input = attack;
-        self.update();
+    fn resize(&mut self, sample_rate: usize, rms_size_ms: f32) {
+        let new_size = ((sample_rate as f32) * (rms_size_ms / 1000.0)) as usize;
+        if new_size != self.buffer.size() {
+            self.buffer.resize(new_size);
+            self.rms = 0.0;
+        }
     }
+    fn process(&mut self, value: f32) -> f32 {
+        let new_rms_sample = value.powi(2);
 
-    fn set_release(&mut self, release: f32) {
-        self.release_input = release;
-        self.update();
+        //remove the oldest rms value, add new one
+        self.rms += -self.buffer.oldest() + new_rms_sample;
+        self.buffer.push(new_rms_sample);
+        (self.rms / self.buffer.size() as f32).sqrt()
     }
 }
 
@@ -132,23 +147,14 @@ pub struct Compressor2 {
     threshold: f32,
     knee: f32,
     ratio: f32,
-    attack: f32,
-    release: f32,
     gain: f32,
-    attack_gain: f32,
-    release_gain: f32,
 
     slope: f32,
-    cv_env: f32,
-    cv_gain: f32,
 
-    pre_smooth: f32,
-    cte_env: f32,
     pre_smooth_gain: f32,
     decoupled_peak_detector: DecoupledPeakDetector,
-    rms_size: usize,
-    rms_buffer: VariableRingBuf,
-    running_rms: f32,
+    rms_size: f32,
+    rms: AccumulatingRMS,
 }
 
 impl Compressor2 {
@@ -158,25 +164,15 @@ impl Compressor2 {
             threshold: 0.0,
             knee: 0.0,
             ratio: 0.0,
-            attack: 0.0,
-            release: 0.0,
             gain: 0.0,
-            attack_gain: 0.0,
-            release_gain: 0.0,
 
             slope: 0.0,
-            cv_env: 1.0,
-            cv_gain: 0.0,
 
-            pre_smooth: 0.0,
-            cte_env: 0.0,
             pre_smooth_gain: 0.0,
             decoupled_peak_detector: DecoupledPeakDetector::new(0.0, 0.0, 48000.0),
 
-            rms_size: 0,
-            rms_buffer: VariableRingBuf::new(10, 1000000),
-
-            running_rms: 0.0,
+            rms_size: 0.0,
+            rms: AccumulatingRMS::new(48000, 5.0, 192000),
         }
     }
 
@@ -197,29 +193,15 @@ impl Compressor2 {
         self.gain = gain_from_db(gain);
         self.threshold = threshold;
         self.knee = knee;
-        self.pre_smooth = pre_smooth;
-
-        self.attack = attack;
-        self.attack_gain = (-2.0 * PI * 1000.0 / attack / sample_rate).exp(); //UNUSED
-
-        self.release = release;
-        self.release_gain = (-2.0 * PI * 1000.0 / release / sample_rate).exp(); //UNUSED
 
         self.slope = 1.0 / self.ratio - 1.0;
-        self.pre_smooth_gain = (-2.0 * PI * 1000.0 / (self.pre_smooth) / sample_rate).exp();
-        self.decoupled_peak_detector.set_attack(attack);
-        self.decoupled_peak_detector.set_release(release);
-        self.decoupled_peak_detector.set_sample_rate(sample_rate);
-
-        let rms_size = rms_size as usize;
+        self.pre_smooth_gain = (-2.0 * PI * 1000.0 / pre_smooth / sample_rate).exp();
+        self.decoupled_peak_detector
+            .update(attack, release, sample_rate);
 
         if rms_size != self.rms_size {
             self.rms_size = rms_size;
-            if self.rms_size >= 1 {
-                self.rms_buffer
-                    .resize((sample_rate * ((self.rms_size as f32) / 1000.0)) as usize);
-                self.running_rms = 0.0;
-            }
+            self.rms.resize(sample_rate as usize, self.rms_size)
         }
     }
 
@@ -227,61 +209,27 @@ impl Compressor2 {
     //detector_input = (input_l + input_r).abs() * 0.5
     //Returns attenuation multiplier
     pub fn process(&mut self, detector_input: f32) -> f32 {
-        if false {
-            // Ballistics filter and envelope generation
-            let cte = if detector_input >= self.envelope {
-                self.attack_gain
-            } else {
-                self.release_gain
-            };
+        let mut detector_input = detector_input;
+        if self.rms_size >= 1.0 {
+            detector_input = self.rms.process(detector_input);
+        }
 
-            self.envelope = detector_input + cte * (self.envelope - detector_input);
-            let env_db = db_from_gain(self.envelope);
+        self.envelope = detector_input + self.pre_smooth_gain * (self.envelope - detector_input);
 
-            let cv = gain_from_db(
-                reiss(env_db, self.threshold, self.knee, self.ratio, self.slope) - env_db,
-            );
-
-            if cv.is_finite() {
-                cv
-            } else {
-                1.0
-            }
+        self.envelope = if self.envelope.is_finite() {
+            self.envelope
         } else {
-            let mut detector_input = detector_input;
-            if self.rms_size >= 1 {
-                let new_rms_sample = detector_input.powi(2);
-                //remove the oldest rms value, add new one
-                self.running_rms = self.running_rms - self.rms_buffer.oldest() + new_rms_sample;
-                self.rms_buffer.push(new_rms_sample);
-                //let mut rms = 0.0;
-                //for i in 0..self.rms_buffer.len() {
-                //    let multiplier = 0.5
-                //        * (1.0
-                //            - (2.0 * PI * (i as f32) / (self.rms_buffer.len() - 1) as f32).cos());
-                //    rms += (multiplier * self.rms_buffer.get(i)).powi(2);
-                //}
-                detector_input = (self.running_rms / self.rms_buffer.size() as f32).sqrt();
-            }
+            1.0
+        };
 
-            self.envelope =
-                detector_input + self.pre_smooth_gain * (self.envelope - detector_input);
+        let db = db_from_gain(self.envelope);
 
-            self.envelope = if self.envelope.is_finite() {
-                self.envelope
-            } else {
-                1.0
-            };
-
-            let db = db_from_gain(self.envelope);
-
-            let mut cv = db - reiss(db, self.threshold, self.knee, self.ratio, self.slope);
-            cv = gain_from_db(-self.decoupled_peak_detector.process_smooth(cv));
-            if cv.is_finite() {
-                cv
-            } else {
-                1.0
-            }
+        let mut cv = db - reiss(db, self.threshold, self.knee, self.ratio, self.slope);
+        cv = gain_from_db(-self.decoupled_peak_detector.process_smooth(cv));
+        if cv.is_finite() {
+            cv
+        } else {
+            1.0
         }
     }
 }
